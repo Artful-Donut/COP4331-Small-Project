312c982c5846c19571e20f2ba4dbd1043a02c31a
Artful-Donut <lynne.nguyen@gmail.com>
2025-02-12 22:12:51 -0500

    I think I did somethign wrong lol

--- contactManagerFolder/LAMPAPI/LoginRegisterAPI/Register.php
+++ contactManagerFolder/LAMPAPI/LoginRegisterAPI/Register.php
@@ -1,3 +1,32 @@
 <?php
+    # Retrieve JSON
+    $inData = getRequestInfo();
+    
+    # Set default values
+    $id = $inData["id"];
+    $firstName = $inData["first_name"];
+    $lastName = $inData["last_name"];
+
+
+
+
+// Function stuff ig
+    function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	};
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
 
 ?>
