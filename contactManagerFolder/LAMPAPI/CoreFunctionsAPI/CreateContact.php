53c69a6d9e4de339c47acbdb3b2d04d859851e1e
Omari Hopkins <ohopkins41@gmail.com>
2025-02-14 01:32:04 -0500

    Succesfully implemented the create contact api

---
+++ contactManagerFolder/LAMPAPI/CoreFunctionsAPI/CreateContact.php
@@ -0,0 +1,62 @@
+<?php
+// Requesting incoming JSON object
+$inData = getRequestInfo();
+
+// Extracting values from JSON
+$fullName = $inData["fullName"];
+$email = $inData["email"];
+$userId = $inData["userId"];
+
+// Connection object that allows us to connect to our db
+$conn = new mysqli("23.20.217.81", "root", "iSf7VogRMo0/", "lampTest");
+
+if ($conn->connect_error)
+{
+    die(json_encode(["error" => "Database connection failed: " . $conn->connect_error]));
+//    returnWithError($conn->connect_error);
+} 
+else
+{
+    // Preparing sql statement to be executed on our DB
+    $stmt = $conn->prepare("INSERT INTO Contacts (FullName, Email, UserId) VALUES (?, ?, ?)");
+    $stmt->bind_param("ssi", $fullName, $email, $userId); // Binding our parameters to the ? arguments in the SQL statement
+
+    // Executing statement
+    if ($stmt->execute()) 
+    {
+        // Success case - Get generated id on the most recently created user to send to the front end
+        $newId = $conn->insert_id; 
+        returnWithSuccess($newId);
+    } 
+    
+    // Fail case - return an error response object to the front end
+    else
+    {
+        returnWithError($stmt->error);
+    }
+
+    // Closing DB
+    $stmt->close();
+    $conn->close();
+}
+
+// Functions
+function getRequestInfo() {
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson($obj) {
+    header('Content-type: application/json');
+    echo $obj;
+}
+
+function returnWithError($err) {
+    $retValue = json_encode(array("error" => $err));
+    sendResultInfoAsJson($retValue);
+}
+
+function returnWithSuccess($newId) {
+    $retValue = json_encode(array("message" => "Contact created successfully", "contactId" => $newId));
+    sendResultInfoAsJson($retValue);
+}
+?>
--- contactManagerFolder/js/crud.js
+++ contactManagerFolder/js/crud.js
@@ -1,14 +1,15 @@
 // Controller level for calling API endpoints in LAMP API
 
-// Url for database
-const url = "http://contact.afari.online/LAMPAPI";
+// Url for endpoints
+const urlBase = "http://contact.afari.online/LAMPAPI";
 const extension = "";
 
 // User attributes
-let userId = 0;
-let fullName = "";
 
+let ID = 0;
+let fullName = "";
 let email = "";
+let UserID = 1; // The ID of the account that is creating the contact
 
 
 // Handling crud operations
@@ -17,12 +18,12 @@ let email = "";
 function createContact()
 {
     // When we test it, these values needs to be hard coded
-    let newContactName = document.getElementById("full-name").value;
-    let newEmailName = document.getElementById("email").value;
+    let newContactName = "Lebron James";
+    let newEmailName = "LeGloriousKing@gmail.com";
 
     // Creating json Payload to be sent to our PHP defined api endpoint
-    let tmpPayload = {FullName:newContactnAME,userId,userId};
-    let jsonPayload = JSON.stringify( tmp );
+    let tmpPayload = {fullName: newContactName, userId: UserId, email: email};
+    let jsonPayload = JSON.stringify( tmpPayload );
 
     // Url where our endpoint is defined
     let url = urlBase + '/CreateContact.' + extension;
@@ -37,14 +38,15 @@ function createContact()
         {
             if (this.readyState == 4 && this.status == 200)
             {
-                alert("Contact submitted successfully")
+                let response = JSON.parse(this.responseText)
+                alert("Contact submitted successfully with ID: " + response.contactId)
             }
         };
         xhr.send(jsonPayload);
     }
     catch(err)
     {
-        document.getElementById("colorAddResult").innerHTML = err.message;
+        alert("Error: " + err.message)
     }
 }
 
