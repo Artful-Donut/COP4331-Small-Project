618602bf034f8dde0a2dc5073521d229f168a3b6
VicRoj123 <victaaa01@gmail.com>
2025-02-13 20:09:17 -0500

    .

--- contactManagerFolder/LAMPAPI/LoginRegisterAPI/Register.php
+++ contactManagerFolder/LAMPAPI/LoginRegisterAPI/Register.php
@@ -9,6 +9,12 @@ $mail = $inData["Email"];
 $phone = $inData["PhoneNumber"];
 $pass = $inData["Password"];*/
 
+// error handling
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+header('Content-Type: application/json');
+
+
 # create connection (copied from Login.php)
 $conn = new mysqli("localhost", "poopoo", "peepee", "SMPROJ");
 
