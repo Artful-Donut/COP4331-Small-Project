4bdac768a6d3341e8c9c00cfd55faae435fa5133
OmariHop <ohopkins41@gmail.com>
2025-02-20 23:50:44 -0500

    slight change

--- contactManagerFolder/LAMPAPI/LoginRegisterAPI/Register.php
+++ contactManagerFolder/LAMPAPI/LoginRegisterAPI/Register.php
@@ -1,17 +1,16 @@
 <?php
 // Allow CORS (Enable cross-origin requests)
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: http://omarihop.xyz"); // Use * if multiple allowed origins
 header("Access-Control-Allow-Methods: POST, GET, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true"); // Allows cookies & authentication
 header("Content-Type: application/json");
 
 // Handle preflight OPTIONS request
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS')
-{
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
 }
-
 
 $inData = getRequestInfo();
 
